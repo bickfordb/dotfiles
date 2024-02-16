0a460d1b55ad86eb0c1478b3785a2f648ffda21a
Brandon Bickford <brandon@tiptop.xyz>
2024-02-16 14:39:20 -0800

    sync

--- setup.sh
+++ setup.sh
@@ -3,16 +3,18 @@
 set -eux
 
 bash install-neovim.sh
-
+apt-get -y install python3-venv
 mkdir -p ~/.config
 ln -sf ${PWD}/config/nvim ~/.config/nvim
 
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+#sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+#       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-nvim -u ~/.config/nvim/plugins.vim --headless +PlugUpdate +PlugInstall +qall
+#nvim -u ~/.config/nvim/plugins.vim --headless +PlugUpdate +PlugInstall +qall
 cat >>~/.profile - <<EOF
 
 alias vi=nvim
 alias vim=nvim
 EOF
+
+pip3 install codespell install python-lsp-server
